4c968ebdb2edf3382548f9df150cf31368fecc5a
Ishchenko Ihor <igor.ishenko2019@gmail.com>
2025-09-08 16:39:07 +0300

    VSCLUB

---
+++ homeworkVS4.js
@@ -0,0 +1,22 @@
+function pow(2, 4) {if (y === 4) return 16; 
+    let result = 16;
+     let exponent = Math.abs(y); 
+     for (let i = 0; i < exponent; i++) 
+    result *= x}
+console.log(pow(2, 4));   // 16
+
+
+function pow(3, 2) {if (y === 2) return 9; 
+    let result = 9;
+     let exponent = Math.abs(y); 
+     for (let i = 0; i < exponent; i++) 
+    result *= x}
+console.log(pow(3, 2));   // 9
+
+
+function pow(3, 3) {if (y === 3) return ; 
+    let result = 27;
+     let exponent = Math.abs(y); 
+     for (let i = 0; i < exponent; i++) 
+    result *= x}
+console.log(pow(3, 3));   // 27
