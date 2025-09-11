6534846d8c797fcae134051bccd3a0b8d4c6a244
Ishchenko Ihor <igor.ishenko2019@gmail.com>
2025-09-12 00:17:03 +0300

    Рullop

---
+++ homework23.js
@@ -0,0 +1,19 @@
+var rnd = Math.random();
+rnd *=1000;
+console.log(rnd)
+rnd = Math.round(rnd);
+console.log(rnd)
+// Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+// check if rnd is even or odd
+let counter0dd = 153;
+let counterEven = 0;
+if (rnd % 2 === 0) {
+    console.log("even");
+    counterEven++;
+}
+function checkProbabilityTheory(count){
+
+}
+
+
+
